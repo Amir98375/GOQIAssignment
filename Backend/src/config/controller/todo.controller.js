30d5fd372ebbdd23e254a53eb60c7d77d077115b
amir <mohd.a@test.com>
2024-03-29 09:54:55 +0530

    error handling

--- Backend/src/config/controller/todo.controller.js
+++ Backend/src/config/controller/todo.controller.js
@@ -12,22 +12,48 @@ router.get("",async(req,res)=>{
 
 router.post("",async(req,res)=>{
     debugger
-    console.log(req,'request my')
-  const todo=await Todo.create(req.body)
-  return res.status(200).send({message:'Data Added Successfully'
-,flag:1})
+    try {
+      const todo = await Todo.create(req.body);
+       
+      return res.status(200).send({ message: 'Data Added Successfully', flag: 1 });
+    } catch (error) {
+
+      console.error(error);
+      return res.status(500).send({ message: 'Internal Server Error', flag: 0 });
+    }
 })
 
 
 router.patch("/:id",async(req,res)=>{
-     const batch= await Todo.findByIdAndUpdate(req.params.id,req.body,{new:true})
-     return res.status(200).send(batch)
+  try {
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!todo) {
+     
+      return res.status(404).send({ message: 'Todo not found', flag: 0 });
+    }
+
+    return res.status(200).send({ message: 'Data Modified Successfully', flag: 1 });
+  } catch (error) {
+    
+    console.error(error);
+    return res.status(500).send({ message: 'Internal Server Error', flag: 0 });
+  }
   })
 
 
   router.delete("/:id",async(req,res)=>{
-      const batch = await Todo.findByIdAndDelete(req.params.id)
-      return res.status(200).send(batch)
+    try {
+      const todo = await Todo.findByIdAndDelete(req.params.id);
+      if (!todo) {
+       
+        return res.status(404).send({ message: 'Todo not found', flag: 0 });
+      }
+  
+      return res.status(200).send({ message: 'Data Deleted Successfully', flag: 1 });
+    } catch (error) {
+   
+      return res.status(500).send({ message: 'Internal Server Error', flag: 0 });
+    }
   })
 
 
